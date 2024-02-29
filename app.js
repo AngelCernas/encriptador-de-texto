a03e1ba03a23297e1ae2d085d682976d5ec53a62
Angel Cernas <angel_210899@hotmail.com>
2024-02-28 19:01:15 -0600

    css terminado e inicios del js

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+function encriptar(){
+            
+    let textoAEncriptar = document.querySelector(elemento)
+    console.log(textoAEncriptar)
+    console.log("encriptar")
+}
+
+function desencriptar(){
+    console.log("desencriptar")
+}
