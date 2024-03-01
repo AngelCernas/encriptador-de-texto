a9b7e55cf6174ee3fec004f42b79af993cfe0510
Angel Cernas <angel_210899@hotmail.com>
2024-02-29 18:48:01 -0600

    ya encripta falta desencriptar, copiar y inabilitar las imagenes

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 function encriptar(){
-    console.log('encriptar');
+    let textvalue = document.getElementById("textoAEncriptar").value;
+
+    let texencriptado = (textvalue.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat") );
+    mensaje.value = texencriptado;
+    document.getElementById("mensaje").setAttribute('disable','true')
 }
 
 function desencriptar(){
     console.log('desencriptar');
 }
+
+function copiarTexto(){
+    let texMen = document.getElementById("msgencript").value;
+
+    console.log(texMen);
+}
+
