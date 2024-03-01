832869dcaaa613a5153b80098666e910b46fb31b
Angel Cernas <angel_210899@hotmail.com>
2024-02-29 19:06:27 -0600

    falta boton copy y habilitar y desabilitar la imagen de fondo

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ function encriptar(){
     document.getElementById("mensaje").setAttribute('disable','true')
 }
 
+
 function desencriptar(){
-    console.log('desencriptar');
+
+    let textvaluedes = document.getElementById("textoAEncriptar").value;
+
+    let texdesencriptado = (textvaluedes.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u") );
+    mensaje.value = texdesencriptado;
+    document.getElementById("mensaje").setAttribute('disable','false')
 }
 
 function copiarTexto(){
     let texMen = document.getElementById("msgencript").value;
+    document.getElementById("butonCopy").setAttribute('disable','true')
 
+    
     console.log(texMen);
 }
-
